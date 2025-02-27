34c241aaff15261cc2d8fd161c887a5c9e9ac6b1
wangqz <2809755637@qq.com>
2025-02-27 17:45:59 +0800

    fix: npm 镜像源

---
+++ start.sh
@@ -0,0 +1,9 @@
+###
+ # @Author: wangqz
+ # @Date: 2025-02-27
+ # @LastEditTime: 2025-02-27
+ # @Description: content
+### 
+
+# npm install -g dotenv-cli
+dotenv -e ./.env -c -- node ./build/index.js
